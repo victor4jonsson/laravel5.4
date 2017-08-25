08740d4fcc7883bef0d692cb1668a73624f8f873
victor4jonsson <victor@cleansoft.br>
2017-08-25 18:50:17 -0300

    arquivosLayout

---
+++ resources/views/estilos.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+    <title>Controle de estoque</title>
+
+</head>
+<body>
+@yield('conteudo')
+
+</body>
+</html
